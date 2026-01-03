4ebb9e38fbea36ef835f617d9b3f660c6878e734
Madushansuriyabandara <madushansuriyabandara@gmail.com>
2026-01-04 02:31:16 +0530

    Use datasourceBuilder() for SSL configuration

--- src/main/java/com/innovest/config/RagConfiguration.java
+++ src/main/java/com/innovest/config/RagConfiguration.java
@@ -73,7 +73,7 @@ public class RagConfiguration {
                 dataSource.setSslMode("require");
             }
 
-            return PgVectorEmbeddingStore.builder()
+            return PgVectorEmbeddingStore.datasourceBuilder()
                     .datasource(dataSource)
                     .table("embeddings")
                     .dimension(384)
