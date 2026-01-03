0340aa9c70783218d4510462dfcdc4be2441be0a
Madushansuriyabandara <madushansuriyabandara@gmail.com>
2026-01-03 12:58:30 +0530

    Setup for local frontned + cloud dev backend development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // if want to revert back the the local local backend, comment out the below code
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://innolink-backend-dev-cpbffwadc4gvdhca.eastus-01.azurewebsites.net',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  // if want to revert back the the local local backend, comment the above code.
 })
